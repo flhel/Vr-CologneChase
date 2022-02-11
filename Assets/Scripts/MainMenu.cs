7f51a0f570a7f69e483bb7d482d3c3d9173211d0
flhel <58229462+flhel@users.noreply.github.com>
2022-02-11 17:40:41 +0100

    ....

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("IntroCutscene");
     }
 
     public void PlayTutorial(){
